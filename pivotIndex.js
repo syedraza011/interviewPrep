8bb8d7cbb7e3bd27054b3d8c113057d510066e46
Syed Raza <syed.raza011@gmail.com>
2022-11-17 22:13:03 -0500

    Question

--- pivotIndex.js
+++ pivotIndex.js
@@ -28,6 +28,8 @@ function pivotIndex(nums) {
 let sumLeft=[]
 for(let i=0;i<nums.length;i++){
 if(nums<sum)
-let pivot=Math.floor(num.length/2)
+let pivot=Math.floor(num.length/2){
+    let left=nums.length
+}
 }
 };
