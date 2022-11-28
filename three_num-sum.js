4f7dcbfbf9408d22053b8cc3eaae5cfc3f86f63b
Syed Raza <syed.raza011@gmail.com>
2022-11-27 19:44:09 -0500

    Question done 3 number sum

--- three_num-sum.js
+++ three_num-sum.js
@@ -1,9 +1,27 @@
 //Three number sum
 console.log(threeSum([12,3,1,2,-6,5,-8,6],0));
 function threeSum(array,target){
-array.sort();
-console.log(array);
-for(let i=0;i<array.length;i++){
-
-}
+    let triplet=[];
+    array.sort((a,b)=>a-b);
+    //console.log(array);
+    for(let i=0;i<array.length-2;i++){
+        let left=i+1;
+        let right=array.length-1
+        
+        while(left < right){
+            currentSum= array[i]+array[left]+array[right];
+            if(currentSum===target){
+                triplet.push([array[i],array[left],array[right]]);
+                left +=1;
+                right -=1;
+            }
+            else if (currentSum<target){
+                left +=1;
+            }
+            else if (currentSum>target){
+                right -=1;
+            }
+        }
+    }
+    return triplet;
 }
